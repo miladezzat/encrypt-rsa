7fc1d6f26f9edf6bc64d538491e9108eb19c2a5a
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-07-19 10:20:10 +0200

    updated

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 import typescript from '@rollup/plugin-typescript';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 export default {
   input: 'src/index.ts',
-  plugins: [typescript()],
+  plugins: [typescript(), nodePolyfills()],
   output: {
     dir: './build',
     format: 'umd',
